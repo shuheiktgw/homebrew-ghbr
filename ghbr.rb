b930a9c1e09ae986ddd589687345493200265aa4
Shuhei Kitagawa <shuheiktgw@users.noreply.github.com>
2018-08-23 09:07:49 +0900

    Bumps up to v0.0.5

--- ghbr.rb
+++ ghbr.rb
@@ -5,7 +5,7 @@ class Ghbr < Formula
   version 'v0.0.5'
 
   url 'https://github.com/shuheiktgw/ghbr/releases/download/v0.0.5/ghbr_v0.0.5_darwin_amd64.zip'
-  sha256 'fc38e34639cc15a81630e28cc55bebdb011d7ffe2cea8a6e0021c9ea99a82efc'
+  sha256 'b2bac2d483fd8984fdd292afba6573c30848d1f1926508c664f734b625ded88c'
 
   def install
     bin.install 'ghbr'
